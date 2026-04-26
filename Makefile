b4821d320862c479fe076f97cb44a8a418f7d07c
Retr0-rt <abdelhamida880@gmail.com>
2026-04-26 22:06:40 +0100

    .

--- Makefile
+++ Makefile
@@ -8,29 +8,22 @@ INCS_DIR    = includes
 OBJS_DIR    = objs
 
 SRCS_FILES  = main.c init.c simulation.c resources.c scheduler.c heap.c monitor.c utils.c cleanup.c
+HEADERS = $(INC_DIR)/codexion.h $(INC_DIR)/scheduler.h
 
-# Advanced mapping: Prepend directories to files
 SRCS        = $(addprefix $(SRCS_DIR)/, $(SRCS_FILES))
 OBJS        = $(addprefix $(OBJS_DIR)/, $(SRCS_FILES:.c=.o))
 
-# Main rule
 all: $(NAME)
 
-# Linking the final executable
 $(NAME): $(OBJS)
 	$(CC) $(CFLAGS) -o $(NAME) $(OBJS)
 
-# Compiling object files
-# The '-I $(INCS_DIR)' is what tells the compiler where your headers are!
-# The '| $(OBJS_DIR)' ensures the objs folder exists before compiling.
-$(OBJS_DIR)/%.o: $(SRCS_DIR)/%.c | $(OBJS_DIR)
+$(OBJS_DIR)/%.o: $(SRCS_DIR)/%.c $(HEADERS) | $(OBJS_DIR)
 	$(CC) $(CFLAGS) -I $(INCS_DIR) -c $< -o $@
 
-# Create the objects directory
 $(OBJS_DIR):
 	mkdir -p $(OBJS_DIR)
 
-# Cleanup rules
 clean:
 	rm -rf $(OBJS_DIR)
 
---
+++ srcs/init.c
@@ -0,0 +1,49 @@
+#include "codexion.h"
+
+
+int init_system_args(t_system *sys, int ac, char **av){
+    sys->nb_coders = atoi(av[0]);
+    sys->time_to_burnout = atoi(av[1]);
+    sys->time_to_compile = atoi(av[2]);
+    sys->time_to_debug = atoi(av[3]);
+    sys->time_to_refactor = atoi(av[4]); 
+    sys->req_compiles = atoi(av[5]);
+    sys->scheduler = av[6];
+
+}
+
+int init_mutexes(t_system *sys)
+{
+    int i;
+
+    i = 0;
+    sys->dongles = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t) * sys->nb_coders);
+    while (i < sys->nb_coders)
+    {
+        if(pthread_mutex_init(sys->dongles + i, NULL) != 0)
+        {
+            printf("error initailizing the dongle %d", i + 1);
+            return (-1);
+        }
+        i++;
+    }
+    return (0);
+}
+
+int init_coders(t_system *sys)
+{
+    int i;
+    struct timeval currrent_timestamp;
+    gettimeofday(&currrent_timestamp, NULL);
+    i = 0;
+    sys->coders = (t_coder*)malloc(sizeof(t_coder) * sys->nb_coders);
+
+    while (i < sys->nb_coders)
+    {
+
+        sys->coders[i].id = i + 1;
+        sys->coders[i].last_compile_start = get
+        i++;
+    }
+
+}

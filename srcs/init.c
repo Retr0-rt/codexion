f64589a0825617545619506f2c5c9df5529a7c72
Retr0-rt <abdelhamida880@gmail.com>
2026-04-26 22:06:54 +0100

    .

--- srcs/init.c
+++ srcs/init.c
@@ -18,6 +18,9 @@ int init_mutexes(t_system *sys)
 
     i = 0;
     sys->dongles = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t) * sys->nb_coders);
+    sys->log_mutex = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+    sys->monitor_mutex = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+
     while (i < sys->nb_coders)
     {
         if(pthread_mutex_init(sys->dongles + i, NULL) != 0)
@@ -27,6 +30,7 @@ int init_mutexes(t_system *sys)
         }
         i++;
     }
+    if(pthread_mutex_init)
     return (0);
 }
 
@@ -42,7 +46,10 @@ int init_coders(t_system *sys)
     {
 
         sys->coders[i].id = i + 1;
-        sys->coders[i].last_compile_start = get
+        sys->coders[i].last_compile_start = currrent_timestamp.tv_sec * 1000 + currrent_timestamp.tv_usec / 1000;
+        sys->coders[i].left_dongle_id = i;
+        sys->coders[i].right_dongle_id = (i + 1) % sys->nb_coders;
+        sys->coders[i].sys = &sys;
         i++;
     }
 

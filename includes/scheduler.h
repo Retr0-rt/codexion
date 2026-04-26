f6f4a759bdf7ffe446e692fe31c85f6d2e35ad6e
Retr0-rt <abdelhamida880@gmail.com>
2026-04-26 22:07:26 +0100

    .

--- includes/scheduler.h
+++ includes/scheduler.h
@@ -1,4 +1,15 @@
 #ifndef SCHEDULER_H
 # define SCHEDULER_H
 
+typedef struct s_heap_node {
+    int         coder_id;
+    long long   priority; // The time value used for sorting
+} t_heap_node;
+
+typedef struct s_heap {
+    t_heap_node *array;
+    int         size;
+    int         capacity;
+} t_heap;
+
 #endif
--- srcs/resources.c
+++ srcs/resources.c
@@ -1,39 +1,36 @@
 #include "codexion.h"
 
-void take_dongles(t_coder *coder)
-{
-    // 1. Blindly grab the Left dongle
-    pthread_mutex_lock(&coder->sys->dongles[coder->left_dongle_id]);
-    
-    pthread_mutex_lock(&coder->sys->log_mutex);
-    if (!safe_stop_check(coder->sys))
-        printf("%lld %d has taken a left dongle\n", 
-            get_relative_time(coder->sys->start_time), coder->id);
-    pthread_mutex_unlock(&coder->sys->log_mutex);
 
-    // 2. Blindly grab the Right dongle
-    pthread_mutex_lock(&coder->sys->dongles[coder->right_dongle_id]);
-    
+static void log_taking_a_dongle(t_coder *coder){
     pthread_mutex_lock(&coder->sys->log_mutex);
     if (!safe_stop_check(coder->sys))
-        printf("%lld %d has taken a right dongle\n", 
+        printf("%lld %d has taken a dongle\n",
             get_relative_time(coder->sys->start_time), coder->id);
     pthread_mutex_unlock(&coder->sys->log_mutex);
 }
 
+void take_dongles(t_coder *coder)
+{
+    if(coder->left_dongle_id < coder->right_dongle_id)
+    {
+        pthread_mutex_lock(&coder->sys->dongles[coder->left_dongle_id]);
+        log_taking_a_dongle(coder);
+
+        pthread_mutex_lock(&coder->sys->dongles[coder->right_dongle_id]);
+        log_taking_a_dongle(coder);
+    }
+    else
+    {
+        pthread_mutex_lock(&coder->sys->dongles[coder->right_dongle_id]);
+        log_taking_a_dongle(coder);
+
+        pthread_mutex_lock(&coder->sys->dongles[coder->left_dongle_id]);
+        log_taking_a_dongle(coder);
+    }
+}
+
 void drop_dongles(t_coder *coder)
 {
-    // Drop both so the next coder can use them
     pthread_mutex_unlock(&coder->sys->dongles[coder->left_dongle_id]);
-    pthread_mutex_lock(&coder->sys->log_mutex);
-    if (!safe_stop_check(coder->sys))
-        printf("%lld %d has dropped a left dongle\n", 
-            get_relative_time(coder->sys->start_time), coder->id);
-    pthread_mutex_unlock(&coder->sys->log_mutex);
-
     pthread_mutex_unlock(&coder->sys->dongles[coder->right_dongle_id]);
-    if (!safe_stop_check(coder->sys))
-        printf("%lld %d has dropped right dongle\n", 
-            get_relative_time(coder->sys->start_time), coder->id);
-    pthread_mutex_unlock(&coder->sys->log_mutex);
 }
